43093f445909fcc450d814a2e4fdc07bbc075425
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-05-02 10:50:49 +0530

    same site problem fix

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -52,7 +52,7 @@ const login = async (req, res) => {
             httpOnly: true,
             secure: isProduction,
             path: "/",
-            sameSite: isProduction ? 'None' : 'Lax',
+            sameSite: isProduction ? 'none' : 'Lax',
             maxAge: 24 * 60 * 60 * 1000,
         });
 
