409bb576f790e7bbb7c0fa680157a76c3ceef727
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-07-19 05:23:40 +0530

    next url

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,18 +1,20 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
-    const isPublicPath = path === '/login' || path === '/signup' || path=== 'forgot-password'
-    const token = request.cookies.get('auth_token')?.value || ''
+    const token = request.cookies.get('auth_token')?.value 
+    const isPublicPath = path === '/login' || path === '/signup' || path === 'forgot-password'
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/admin/dashboard', request.url))
     }
 
-    if (!isPublicPath && !token) {    
+    if (!isPublicPath && !token) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
+
+    return NextResponse.next()
 }
 
 export const config = {
